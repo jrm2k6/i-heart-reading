4c146ee703d9a076ae60c59673582f8b6d2b899e
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-01-08 11:20:49 -0800

    update template

---
+++ resources/assets/js/student-app.js
@@ -0,0 +1,4 @@
+(function() {
+    var ReactDOM = require('react-dom');
+    ReactDOM.render(<div>Hello People</div>, document.getElementById("student-app-container"));
+}());
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container spark-screen">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div id="student-app-container"></div>
+    <script></script>
+@endsection
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('js')
+    <script src="{{ elixir('js/student-app.js') }}"></script>
 @endsection
