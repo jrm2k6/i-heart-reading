ce770417b5c6f3ea15ca3d1b787d2bafb1d8e79d
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-01-26 17:42:47 -0800

    wip

--- resources/assets/js/student-app.js
+++ resources/assets/js/student-app.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import AppComponent from './components/AppComponent';
 
+import ReactRouter from 'react-router';
+
 (function() {
     ReactDOM.render(<AppComponent />, document.getElementById("student-app-container"));
 }());
