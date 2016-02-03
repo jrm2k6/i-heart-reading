df9525c8c25e24a830bc83c07c0bc0652ce8e0e0
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-02-03 09:26:32 -0800

    simple form for adding a book

--- resources/assets/js/components/AddBookComponent.js
+++ resources/assets/js/components/AddBookComponent.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+import {FormsyDate, FormsyText} from 'formsy-material-ui';
+import {Form} from 'formsy-react';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 export default class BookComponent extends React.Component {
     render() {
-        return (
-            <div>
-              Add Book component
-            </div>
-        );
+      return (
+        <Form>
+          <FormsyText
+            name='book-title'
+            floatingLabelText='Title'
+          />
+          <FormsyText
+            name='book-author-name'
+            floatingLabelText='Author'
+          />
+          <div>
+            <FormsyText
+              name='book-nb-pages-read'
+            />
+            <span>of</span>
+            <FormsyText
+              name='book-nb-pages'
+            />
+          </div>
+          <RaisedButton
+            type="submit"
+            label="Submit"
+          />
+        </Form>
+      );
     }
 }
