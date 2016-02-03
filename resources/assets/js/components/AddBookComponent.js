0fb03e59a577a8f93369100d09c12ff0fad151f0
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-02-03 09:39:03 -0800

    add basic validation

--- resources/assets/js/components/AddBookComponent.js
+++ resources/assets/js/components/AddBookComponent.js
@@ -5,29 +5,50 @@ import {Form} from 'formsy-react';
 import RaisedButton from 'material-ui/lib/raised-button';
 
 export default class BookComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          canSubmit: false
+        };
+    }
+
+    submit() {
+      console.log('submit');
+    }
+
     render() {
       return (
-        <Form>
+        <Form onValidSubmit={this.submit}
+            onValid={() => {this.setState({canSubmit: true})}}
+            onInvalid={() => {this.setState({canSubmit: false})}}
+        >
           <FormsyText
             name='book-title'
+            required
             floatingLabelText='Title'
           />
           <FormsyText
             name='book-author-name'
+            required
             floatingLabelText='Author'
           />
           <div>
             <FormsyText
               name='book-nb-pages-read'
+              value='0'
+              validations='isInt'
             />
             <span>of</span>
             <FormsyText
               name='book-nb-pages'
+              required
+              validations='isInt'
             />
           </div>
           <RaisedButton
             type="submit"
             label="Submit"
+            disabled={!this.state.canSubmit}
           />
         </Form>
       );
