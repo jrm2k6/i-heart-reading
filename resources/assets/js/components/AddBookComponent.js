57706078407a45072e3cb6fde1c582175ed6ccb4
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-02-25 18:19:43 -0800

    fix indentation + change layout

--- resources/assets/js/components/AddBookComponent.js
+++ resources/assets/js/components/AddBookComponent.js
@@ -8,55 +8,66 @@ import SearchBookComponent from './SearchBookComponent';
 import { createBook } from '../actions/crudActions';
 
 export default class AddBookComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        canSubmit: false
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      canSubmit: false,
+      showAddBook: false
+    };
+  }
 
-    submit(_data) {
-      this.props.onAddBook(_data);
-    }
+  submit(_data) {
+    this.props.onAddBook(_data);
+  }
 
-    render() {
-      return (
+  render() {
+    let addBookForm = (this.state.showAddBook) ?
+      this.getAddBookForm() : null;
+
+    return (
+      <div>
+        <SearchBookComponent />
+        <span onClick={() => {this.setState({ showAddBook: true })}}>Cannot find your book? Click to add your new book</span>
+        {addBookForm}
+      </div>
+    );
+  }
+
+  getAddBookForm() {
+    return (
+      <Form onValidSubmit={(data) => {this.submit(data);}}
+          onValid={() => { this.setState({ canSubmit: true });}}
+          onInvalid={() => { this.setState({ canSubmit: false });}}
+      >
+        <FormsyText
+          name='book_title'
+          required
+          floatingLabelText='Title'
+        />
+        <FormsyText
+          name='book_author_name'
+          required
+          floatingLabelText='Author'
+        />
         <div>
-          <SearchBookComponent />
-          <Form onValidSubmit={(data) => {this.submit(data);}}
-              onValid={() => { this.setState({ canSubmit: true });}}
-              onInvalid={() => { this.setState({ canSubmit: false });}}
-          >
-            <FormsyText
-              name='book_title'
-              required
-              floatingLabelText='Title'
-            />
-            <FormsyText
-              name='book_author_name'
-              required
-              floatingLabelText='Author'
-            />
-            <div>
-              <FormsyText
-                name='book_nb_pages_read'
-                value='0'
-                validations='isInt'
-              />
-              <span>of</span>
-              <FormsyText
-                name='book_nb_pages'
-                required
-                validations='isInt'
-              />
-            </div>
-            <RaisedButton
-              type='submit'
-              label='Submit'
-              disabled={!this.state.canSubmit}
-            />
-          </Form>
+          <FormsyText
+            name='book_nb_pages_read'
+            value='0'
+            validations='isInt'
+          />
+          <span>of</span>
+          <FormsyText
+            name='book_nb_pages'
+            required
+            validations='isInt'
+          />
         </div>
-      );
-    }
+        <RaisedButton
+          type='submit'
+          label='Submit'
+          disabled={!this.state.canSubmit}
+        />
+      </Form>
+    );
+  }
 }
