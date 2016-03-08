f612fa9337aad3d9aa69112690b6b04057972f28
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-03-08 08:43:22 -0800

    add soft deleting to assignments

--- app/Models/BookAssignment.php
+++ app/Models/BookAssignment.php
@@ -1,14 +1,19 @@
 <?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookAssignment extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'book_user';
 
     protected $fillable = [
         'user_id', 'book_id'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function user()
     {
