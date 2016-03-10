b3686481030271c42ae4b96bf6097bdde6d38e5e
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-03-10 14:56:29 -0800

    add semicolon!

--- resources/assets/js/components/BookDashboardComponent.js
+++ resources/assets/js/components/BookDashboardComponent.js
@@ -161,6 +161,6 @@ const EditableAssignmentItem = ({ properties, onDeleteAssignedBook, onClickSave 
       </td>
     </tr>
   );
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookComponent);
