0cfe6f39b35d82f7b6ff7c1670075a6ceeb7d0ae
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-04-07 09:42:22 -0700

    add save button to write button response

--- resources/assets/js/components/responses/WriteResponseComponent.js
+++ resources/assets/js/components/responses/WriteResponseComponent.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { MarkdownEditor } from 'react-markdown-editor';
+import FlatButton from 'material-ui/lib/flat-button';
 
 class WriteResponseComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      responseContent: null
+    };
+  }
+
+  handleOnContentChange(content) {
+    this.setState({ responseContent: content });
+  }
+
   render() {
     return (
-        <div>
+        <div className='write-response-container'>
+          <div>
+            Response for Book
+          </div>
           <MarkdownEditor initialContent=''
             iconsSet='font-awesome'
+            onContentChange={(content) => {this.handleOnContentChange(content);}}
           />
+        <div className='response-actions'>
+          <FlatButton
+            label='Save' primary
+            className='save-response-button'
+          />
+        </div>
         </div>
     );
   }
