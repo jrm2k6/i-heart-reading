6190dba81c144ec825f8a627b916ed3a5c28dd57
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-04-10 13:27:34 -0700

    add helper function on response type

--- app/Models/ResponseType.php
+++ app/Models/ResponseType.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ResponseType extends Model
 {
+    const REQUIRING_URLS = ['video', 'link'];
+
     protected $table = 'response_types';
     public $timestamps = false;
+
+    public function requiresUrl()
+    {
+        return collect(self::REQUIRING_URLS)->contains($this->name);
+    }
 }
