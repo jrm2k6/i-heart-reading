cd11e83440bc9be8d6b676a1e71e64820a33ed70
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-04-16 15:35:38 -0700

    create migration for assignment updates

---
+++ database/migrations/2016_04_16_223322_create_assignment_updates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssignmentUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_updates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('assignment_id')->unsigned();
+            $table->integer('num_pages')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('assignment_id')->references('id')->on('book_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assignment_updates');
+    }
+}
