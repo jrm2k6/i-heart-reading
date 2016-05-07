2cc1439d6f63bd2fabe64aebc76693a90e53a385
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-05-07 16:36:30 -0700

    remove bootstrap dependency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
      <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 </head>
 <body id="app-layout">
@@ -20,7 +19,6 @@
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
     @yield('js')
 </body>
