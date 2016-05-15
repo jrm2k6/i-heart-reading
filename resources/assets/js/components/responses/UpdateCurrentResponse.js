acb6ae08fda15b9e0abf74ef6cf0432ba633b6a8
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-05-14 17:39:20 -0700

    add update current response component

---
+++ resources/assets/js/components/responses/UpdateCurrentResponse.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { markdown } from 'markdown';
+import { fetchAssignedBooks } from '../../actions/crudActions';
+
+const mapStateToProps = (state) => {
+  return {
+    assignedBooks: state.bookReducers.assignedBooks
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchAssignedBooks: () => {
+      dispatch(fetchAssignedBooks());
+    }
+  }
+}
+
+class UpdateResponseComponent extends Component {
+  componentWillMount() {
+    this.props.onFetchAssignedBooks();
+  }
+
+  getCurrentAssignment() {
+    const { assignedBooks } = this.props;
+    const assignmentId = this.props.params.assignmentId;
+
+    return assignedBooks.find(assignedBook => assignedBook.id === parseInt(assignmentId, 10));
+  }
+
+  getCurrentReviewComponent(currentReview) {
+    const iconStatus = (currentReview.decision_type_name === 'accepted') ?
+        (<i className='material-icons accepted'>thumb_up</i>)
+      : (<i className='material-icons rejected'>thumb_down</i>);
+
+    const commentComponent = (currentReview.comment) ?
+      ( <div className='update-response-current-review-comment'>
+          {currentReview.comment}
+        </div>
+      ) : (
+        <div className='update-response-current-review-comment no-comment'>
+          No comment posted for this assignment
+        </div>
+      );
+
+    return (
+      <div className='update-response-current-review'>
+        <div className='update-response-current-review-status'>
+          {iconStatus}
+        </div>
+        {commentComponent}
+      </div>
+    );
+  }
+
+  getCurrentResponse(currentAssignment) {
+    return currentAssignment.response;
+  }
+
+  getResponseComponent(currentResponse) {
+    switch (currentResponse.response_type_id) {
+      case 1:
+        const content = { __html: markdown.toHTML(currentResponse.content) };
+        return (
+          <div className='textual-response-content' dangerouslySetInnerHTML={content}>
+          </div>
+        );
+      case 2:
+        return (
+          <div className='image-response-content'>
+            <img src={currentResponse.url} />
+          </div>
+        );
+      case 3:
+        return (
+          <div className='video-response-content'>
+            <a href={currentResponse.url} target='_blank'>Open Video</a>
+          </div>
+        );
+      case 4:
+        return (
+          <div className='link-response-content'>
+            <a href={currentResponse.url}>Url</a>
+          </div>
+        );
+      default:
+        return (
+          <div>Not able to parse response</div>
+        );
+    }
+  }
+
+  render() {
+    if (this.props.assignedBooks.length > 0) {
+      let responseComponent = null;
+      let currentReviewComponent = null;
+
+      const currentAssignment = this.getCurrentAssignment();
+      const currentResponse = this.getCurrentResponse(currentAssignment);
+      const currentReview = currentAssignment.current_review;
+
+      if (currentResponse) {
+        responseComponent = this.getResponseComponent(currentResponse);
+      }
+
+      if (currentReview) {
+        currentReviewComponent = this.getCurrentReviewComponent(currentReview);
+      }
+
+      const headerTitle = (currentAssignment) ?
+        (<div className='review-response-header-title'>
+          {currentAssignment.book.title} - {currentAssignment.book.author}
+        </div>)
+        : null;
+
+      return (
+          <div className='review-response-container'>
+            <div className='review-response-header-container'>
+              {headerTitle}
+            </div>
+            {responseComponent}
+            {currentReviewComponent}
+          </div>
+      );
+    }
+
+    return null;
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateResponseComponent);
