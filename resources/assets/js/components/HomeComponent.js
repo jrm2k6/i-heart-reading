a9dee9ed8f00623bd35e8d97963f00b952f9e734
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-05-15 17:09:48 -0700

    work in progress for front page

--- resources/assets/js/components/HomeComponent.js
+++ resources/assets/js/components/HomeComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import ItemProgression from './stats/ItemProgression';
-import StatsComponent from './stats/StatsComponent';
+import rd3 from 'rd3';
 import { connect } from 'react-redux';
 import { fetchStats } from '../actions/crudActions';
+
+const PieChart = rd3.PieChart;
 
 
 const mapStateToProps = (state) => {
@@ -18,21 +19,122 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 class HomeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeView: 'yearly'
+    };
+  }
+
   componentDidMount() {
     this.props.fetchStats();
   }
 
+  getPercentageValue(valueToCheck, goal, max = 100) {
+    let _value = Math.round(valueToCheck * max / goal);
+    _value = (_value >= max) ? max : _value;
+    const _remaining = (_value === max) ? 0 : max - _value;
+
+    return {
+      done: _value,
+      remaining: _remaining
+    };
+  }
+
+  getDataForView(stats) {
+    let percentages = null;
+    const goalDailyPages = 25;
+
+    switch (this.state.timeView) {
+      case 'daily':
+        percentages = this.getPercentageValue(stats.daily.num_pages_read, goalDailyPages);
+
+        return [{
+          label: 'Pages read',
+          value: percentages.done
+        },
+        {
+          label: 'Pages to read',
+          value: percentages.remaining
+        }];
+
+      case 'weekly':
+        percentages = this.getPercentageValue(stats.weekly.num_pages_read, goalDailyPages);
+        return [{
+          label: 'Pages read',
+          value: percentages.done
+        },
+        {
+          label: 'Pages to read',
+          value: percentages.remaining
+        }];
+
+      case 'monthly':
+        percentages = this.getPercentageValue(stats.monthly.num_pages_read, goalDailyPages);
+        return [{
+          label: 'Pages read',
+          value: percentages.done
+        },
+        {
+          label: 'Pages to read',
+          value: percentages.remaining
+        }];
+
+      case 'yearly':
+        percentages = this.getPercentageValue(stats.yearly.num_pages_read, goalDailyPages);
+        return [{
+          label: 'Pages read',
+          value: percentages.done
+        },
+        {
+          label: 'Pages to read',
+          value: percentages.remaining
+        }];
+
+
+      default:
+        return {};
+    }
+  }
+
+  getColors() {
+    switch (this.state.timeView) {
+      case 'daily':
+        return idx => ['#ffc400', '#babec3'][idx];
+      case 'weekly':
+        return idx => ['#38a4dd', '#babec3'][idx];
+      case 'monthly':
+        return idx => ['#b39ddb', '#babec3'][idx];
+      case 'yearly':
+        return idx => ['#ffc400', '#babec3', '#b39ddb', '#babec3'][idx];
+      default:
+        return idx => ['#ffc400', '#babec3'][idx];
+    }
+  }
+
   render() {
-    const numPagesReadYear = (this.props.stats.yearly) ?
-      this.props.stats.yearly.num_pages_read :
-      0;
+    const { stats } = this.props;
+    const pieData = (Object.keys(stats).length > 0) ? this.getDataForView(stats) : [];
     return (
       <div className='home-component-container'>
-        <ItemProgression stats={this.props.stats.daily} type='Today' />
-        <ItemProgression stats={this.props.stats.weekly} type='This Week' />
-        <ItemProgression stats={this.props.stats.monthly} type='This Month' />
-        <ItemProgression stats={this.props.stats.yearly} type='This Year' />
-        <StatsComponent numPagesReadYear={numPagesReadYear} />
+        <div className='home-component-pie-chart-container'>
+          <div className='home-component-pie-chart-header'>
+            <span>My progress</span>
+          </div>
+          <div className='home-component-pie-chart'>
+            <PieChart
+              data={pieData}
+              width={450}
+              height={400}
+              radius={110}
+              innerRadius={70}
+              showOuterLabels={false}
+              showTooltip={false}
+              sectorBorderColor='white'
+              colors={this.getColors()}
+            />
+          </div>
+        </div>
       </div>
     );
   }
