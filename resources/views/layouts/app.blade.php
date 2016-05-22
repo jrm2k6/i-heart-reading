a52453c92e5ec8331f0d301de6940db5ab303353
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-05-22 12:33:42 -0700

    show heart icon in title page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>I Heart Reading</title>
+    <title>I &hearts; Reading</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
