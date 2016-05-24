3e0fdd2966b0fb49f3615e806a2f0111f2c2577f
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-05-24 07:33:45 -0700

    displays form errors on register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,28 @@
 
     <form role="form" class="register-form" method="POST" action="{{ url('/register') }}">
         {!! csrf_field() !!}
-
+        @if ($errors->count() > 0)
+            @if ($errors->has('name'))
+                <div class="alert error">
+                    {{ $errors->first('name') }}
+                </div>
+            @endif
+            @if ($errors->has('email'))
+                <div class="alert error">
+                    {{ $errors->first('email') }}
+                </div>
+            @endif
+            @if ($errors->has('password'))
+                <div class="alert error">
+                    {{ $errors->first('password') }}
+                </div>
+            @endif
+            @if ($errors->has('password_confirmation'))
+                <div class="alert error">
+                    {{ $errors->first('password_confirmation') }}
+                </div>
+            @endif
+        @endif
         <div>
             <div class="input-with-picture" tabindex="0">
                 <input type="text" class="form-input"
@@ -17,11 +38,6 @@
                 >
                 <div class="form-input-icon"><img src="/images/icons/people.png" /></div>
             </div>
-            @if ($errors->has('name'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </span>
-            @endif
         </div>
 
         <div>
@@ -32,11 +48,6 @@
                 >
                 <div class="form-input-icon"><img src="/images/icons/email.png" /></div>
             </div>
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
         </div>
 
         <div>
@@ -44,11 +55,6 @@
                 <input type="password" class="form-input" name="password" placeholder="Your password">
                 <div class="form-input-icon"><img src="/images/icons/lock.png" /></div>
             </div>
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
         </div>
 
         <div>
@@ -58,12 +64,6 @@
                 >
                 <div class="form-input-icon"><img src="/images/icons/lock.png" /></div>
             </div>
-
-            @if ($errors->has('password_confirmation'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                </span>
-            @endif
         </div>
 
         <button type="submit" class="btn-register-submit">
