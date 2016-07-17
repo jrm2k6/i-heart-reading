2e3562ca9c45bea7394a280562643caf5982f204
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-07-17 10:49:13 -0700

    add confirmation emails error template

---
+++ resources/views/errors/confirmation_emails_error.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="login-page-container">
+        <div class="logo-container">
+            <img src="/images/logos/i-heart-reading-logo.png" />
+        </div>
+
+        <form class="login-form" role="form" method="POST" action="{{ url('/resend-confirmation-email') }}">
+            {!! csrf_field() !!}
+            @if ($errors->count() > 0)
+                <div class="alert error">
+                    {{$errors->first('email')}}
+                </div>
+            @endif
+            <div>
+                <div class="input-with-picture">
+                    <input type="email" class="form-input"
+                           name="email" value="{{ old('email') }}"
+                           placeholder="mark.twain@email.com"
+                    >
+                    <div class="form-input-icon"><img src="/images/icons/email.png" /></div>
+                </div>
+            </div>
+
+            <button type="submit" class="btn-login-submit">
+                Resend Confirmation Email
+            </button>
+        </form>
+    </div>
+@endsection
