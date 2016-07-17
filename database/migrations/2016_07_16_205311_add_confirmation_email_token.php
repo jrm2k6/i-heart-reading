5e715f877b83d90daf42b9fbabc6a0aa02fe9910
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-07-16 20:58:27 -0700

    add migrations for confirming emails

---
+++ database/migrations/2016_07_16_205311_add_confirmation_email_token.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConfirmationEmailToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_confirmations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('email_confirmations');
+    }
+}
---
+++ database/migrations/2016_07_16_205327_add_confirmed_field_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConfirmedFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->boolean('confirmed')->default(false)->after('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('confirmed');
+        });
+    }
+}
