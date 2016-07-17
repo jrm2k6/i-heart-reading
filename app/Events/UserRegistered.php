cdd3fdb6998c8935c16553d4ccf885ba0aa9c5f9
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-07-17 09:36:51 -0700

    keep reference to user passed to event

--- app/Events/UserRegistered.php
+++ app/Events/UserRegistered.php
@@ -3,6 +3,7 @@
 namespace App\Events;
 
 use App\Events\Event;
+use App\Models\User;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
@@ -10,14 +11,16 @@ class UserRegistered extends Event
 {
     use SerializesModels;
 
+    public $user;
+
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
