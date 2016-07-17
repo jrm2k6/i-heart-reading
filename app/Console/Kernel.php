2df681fcb4f4f3d47fcf1ffd79b3e9e34f35d655
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-07-17 09:36:39 -0700

    add test command to send email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-         Commands\UpdateStatsUser::class,
+        Commands\UpdateStatsUser::class,
+        Commands\SendTestEmail::class,
     ];
 
     /**
