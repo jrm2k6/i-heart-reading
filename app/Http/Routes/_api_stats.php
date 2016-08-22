86a93ad523a6a76f47c04b2872ffae8e2ca9deef
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-08-21 18:45:37 -0700

    add endpoint to fetch stats of student

--- app/Http/Routes/_api_stats.php
+++ app/Http/Routes/_api_stats.php
@@ -2,4 +2,5 @@
 
 Route::group(['prefix' => 'api', 'middleware' => 'web'], function() {
     Route::get('stats/me', 'StatsController@getStats');
+    Route::get('stats/{studentId}', 'StatsController@getStatsForStudent');
 });
