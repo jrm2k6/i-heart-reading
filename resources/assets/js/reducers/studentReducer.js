9efcef770c389d4b677d345420ef90b39dcfe19b
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-08-24 19:00:16 -0700

    handle success updates fetched

--- resources/assets/js/reducers/studentReducer.js
+++ resources/assets/js/reducers/studentReducer.js
@@ -4,6 +4,7 @@ import {
 
 import {
   SUCCESS_STATS_FETCHED,
+  SUCCESS_UPDATES_FETCHED
 } from '../actions/crudActions';
 
 const initialState = {
@@ -16,6 +17,21 @@ function updateCurrentStudent(currentStudent, action) {
   return Object.assign({}, currentStudent, {
     stats: action.payload.stats
   });
+}
+
+function updateCurrentStudentUpdates(currentStudent, action) {
+  return Object.assign({}, currentStudent, {
+    updates: action.payload.updates
+  });
+}
+
+function updateStudentUpdates(stateUpdates, action) {
+  const stats = stateUpdates.filter(studentStats =>
+    Object.keys(studentStats)[0] !== action.payload.studentId);
+
+  const newItem = {};
+  newItem[action.payload.studentId] = action.payload.updates;
+  return stats.concat(newItem);
 }
 
 function updateStats(stateStats, action) {
@@ -38,6 +54,12 @@ export default function studentReducer(state = initialState, action) {
       return Object.assign({}, state, {
         currentStudent: updateCurrentStudent(state.currentStudent, action),
         stats: updateStats(state.stats, action)
+      });
+
+    case SUCCESS_UPDATES_FETCHED:
+      return Object.assign({}, state, {
+        currentStudent: updateCurrentStudentUpdates(state.currentStudent, action),
+        stats: updateStudentUpdates(state.updates, action)
       });
 
     default:
