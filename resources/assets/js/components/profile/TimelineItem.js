34be599da59de706ffc5ff88c5a231b707e008c5
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-08-29 13:58:21 -0700

    differentiate between response items and regular updates

--- resources/assets/js/components/profile/TimelineItem.js
+++ resources/assets/js/components/profile/TimelineItem.js
@@ -4,15 +4,24 @@ import moment from 'moment';
 class TimelineItem extends React.Component {
   getUpdateContent() {
     const { update } = this.props;
-    const book = update.assignment.book;
-    const wasMarkedAsRead = update.mark_book_read === 1;
-    const numPagesRead = update.num_pages;
-    const when = moment(update.updated_at).fromNow();
+    if (update.assignment) {
+      const book = update.assignment.book;
+      const wasMarkedAsRead = update.mark_book_read === 1;
+      const numPagesRead = update.num_pages;
+      const when = moment(update.updated_at).fromNow();
 
-    if (wasMarkedAsRead) {
+      if (wasMarkedAsRead) {
+        return (
+          <div>
+            <span>{book.title} by {book.author} was marked as read</span>
+            <span className='update-row-readable-time'>({when})</span>
+          </div>
+        );
+      }
+
       return (
         <div>
-          <span>{book.title} by {book.author} was marked as read</span>
+          <span>Read {numPagesRead} of {book.title} by {book.author}</span>
           <span className='update-row-readable-time'>({when})</span>
         </div>
       );
@@ -20,8 +29,7 @@ class TimelineItem extends React.Component {
 
     return (
       <div>
-        <span>Read {numPagesRead} of {book.title} by {book.author}</span>
-        <span className='update-row-readable-time'>({when})</span>
+        <span>Submitted a response for {update.book.title} by {update.book.author}</span>
       </div>
     );
   }
