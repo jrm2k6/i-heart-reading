b5359edafbaa5909d50a3f723fe28b511592a70c
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-10-13 19:53:43 -0700

    add placeholders for unit tests

---
+++ tests/unit/Commands/UpdateStatsUserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class UpdateStatsUserTest extends TestCase
+{
+    public function testUserWithoutUpdatesReturnsZeroForBothValues()
+    {
+    }
+
+    public function testStatsWithUpdatesToday()
+    {
+    }
+
+    public function testStatsWithUpdatesThisWeek()
+    {
+    }
+
+    public function testStatsWithUpdatesThisMonth()
+    {
+    }
+
+    public function testStatsWithNoUpdatesToday()
+    {
+    }
+
+    public function testStatsWithNoUpdatesThisWeek()
+    {
+    }
+
+    public function testStatsWithNoUpdatesThisMonth()
+    {
+    }
+}
