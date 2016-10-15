2ff413d222b09da035e8e3dc7da2a47c60f711e3
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-10-14 21:08:44 -0700

    add UserFactory

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+        'type' => 'student'
+    ];
+});
