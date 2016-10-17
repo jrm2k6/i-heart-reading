3732822ad7f522cde69d9d724e2bd12081a67391
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-10-16 18:25:28 -0700

    store previous assignment id and cache it when creating a new update

--- app/Listeners/StudentAssignmentEnded/CreateAssignmentEnded.php
+++ app/Listeners/StudentAssignmentEnded/CreateAssignmentEnded.php
@@ -7,6 +7,7 @@ use App\Events\StudentAssignmentEnded;
 use App\Models\AssignmentUpdate;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Cache;
 
 class CreateAssignmentEnded
 {
@@ -28,11 +29,27 @@ class CreateAssignmentEnded
      */
     public function handle(StudentAssignmentEnded $event)
     {
+        $assignmentId = $event->assignmentId;
+        $previousUpdateId = $this->findPreviousUpdate($assignmentId);
         $assignmentUpdate = new AssignmentUpdate;
-        $assignmentUpdate->assignment_id = $event->assignmentId;
+        $assignmentUpdate->assignment_id = $assignmentId;
         $assignmentUpdate->mark_book_read = true;
         $assignmentUpdate->save();
 
+        Cache::forget('previous_assignment_id_for_' . $assignmentId);
+        Cache::forever('previous_assignment_id_for_' . $assignmentId, $assignmentUpdate->id);
+
         event(new AssignmentEndedUpdateCreated($event->assignmentId));
+    }
+
+    private function findPreviousUpdate($assignmentId) {
+        $previousAssignmentIdCached = Cache::get('previous_assignment_id_for_' . $assignmentId);
+        if ($previousAssignmentIdCached !== null) {
+            return $previousAssignmentIdCached;
+        } else {
+            $ascUpdates = AssignmentUpdate::where('assignment_id', $assignmentId)
+                ->orderBy('created_at', 'DESC')->get()->last();
+            return $ascUpdates;
+        }
     }
 }
--- app/Listeners/StudentAssignmentUpdated/CreateAssignmentUpdate.php
+++ app/Listeners/StudentAssignmentUpdated/CreateAssignmentUpdate.php
@@ -7,6 +7,7 @@ use App\Events\StudentAssignmentUpdated;
 use App\Models\AssignmentUpdate;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Cache;
 
 class CreateAssignmentUpdate
 {
@@ -28,11 +29,28 @@ class CreateAssignmentUpdate
      */
     public function handle(StudentAssignmentUpdated $event)
     {
+        $assignmentId = $event->assignmentId;
+        $previousUpdateId = $this->findPreviousUpdate($assignmentId);
         $assignmentUpdate = new AssignmentUpdate;
-        $assignmentUpdate->assignment_id = $event->assignmentId;
+        $assignmentUpdate->assignment_id = $assignmentId;
         $assignmentUpdate->num_pages = $event->nbPages;
+        $assignmentUpdate->previous_assignment_id = $previousUpdateId;
         $assignmentUpdate->save();
 
-        event(new AssignmentUpdateCreated($event->assignmentId));
+        Cache::forget('previous_assignment_id_for_' . $assignmentId);
+        Cache::forever('previous_assignment_id_for_' . $assignmentId, $assignmentUpdate->id);
+        
+        event(new AssignmentUpdateCreated($assignmentId));
+    }
+
+    private function findPreviousUpdate($assignmentId) {
+        $previousAssignmentIdCached = Cache::get('previous_assignment_id_for_' . $assignmentId);
+        if ($previousAssignmentIdCached !== null) {
+            return $previousAssignmentIdCached;
+        } else {
+            $ascUpdates = AssignmentUpdate::where('assignment_id', $assignmentId)
+                ->orderBy('created_at', 'DESC')->get()->last();
+            return $ascUpdates;
+        }
     }
 }
