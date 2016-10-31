7395043f56978e680ee2f1b6d52d452d4c23a7e0
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-10-31 09:29:38 -0700

    add command to update past updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\UpdateStatsUser::class,
         Commands\SendTestEmail::class,
+        Commands\OneTimeCommands\AttachPreviousAssignmentUpdateId::class
     ];
 
     /**
