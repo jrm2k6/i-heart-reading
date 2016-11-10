270ae2952c898680f0394eda157e362c1ea8f736
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-11-10 09:03:11 -0800

    fix factory for school

--- database/factories/SchoolFactory.php
+++ database/factories/SchoolFactory.php
@@ -3,7 +3,7 @@
 $factory->defineAs(App\Models\School::class, 'default', function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'address' => $faker->name,
+        'address' => $faker->address,
         'domain_name' => 'dragon.com'
     ];
 });
