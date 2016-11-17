2ebdc7bd61e78496680dba6bc4c7ae54c9c737e7
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-11-17 09:02:50 -0800

    add password verification for logged in user actions

--- resources/assets/js/actions/signup/signupContactActions.js
+++ resources/assets/js/actions/signup/signupContactActions.js
@@ -3,12 +3,16 @@ import { displaySuccessAlert, displayErrorAlert } from '../alertsActions';
 
 const SIGNUP_SCHOOL_CONTACT_URL = '/api/school/contact';
 const SIGNUP_VERIFY_CONTACT_URL = '/api/school/contact/verify';
+const SIGNUP_VERIFY_PASSWORD_CURRENT_USER = '/api/me/password/verify';
 const CREATE_CONTACT = 'CREATE_CONTACT';
 export const CONTACT_CREATED = 'CONTACT_CREATED';
 export const ERROR_CONTACT_CREATED = 'ERROR_CONTACT_CREATED';
 const VERIFY_CONTACT = 'VERIFY_CONTACT';
 export const CONTACT_VERIFIED = 'CONTACT_VERIFIED';
 export const ERROR_CONTACT_VERIFIED = 'ERROR_CONTACT_VERIFIED';
+const VERIFY_PASSWORD = 'VERIFY_PASSWORD';
+export const PASSWORD_VERIFIED = 'PASSWORD_VERIFIED';
+export const ERROR_PASSWORD_VERIFIED = 'ERROR_PASSWORD_VERIFIED';
 
 const csrfToken = [].slice.call(document.getElementsByTagName('meta'))
     .filter((meta) => meta.name === 'csrf-token')[0].content;
@@ -65,6 +69,31 @@ function contactVerified(data) {
 function errorContactVerified(data) {
   return {
     type: ERROR_CONTACT_CREATED,
+    data: data.errors
+  };
+}
+
+export function verifyPassword() {
+  return (dispatch, getState) => {
+    const primaryContact = getState().signupReducer.currentPrimaryContact;
+    return apiActions.getRequest(SIGNUP_VERIFY_PASSWORD_CURRENT_USER,
+      { password: password }).then(
+        res => dispatch(passwordVerified(res)),
+        err => dispatch(errorPasswordVerified(err))
+    );
+  }
+}
+
+function passwordVerified(data) {
+  return {
+    type: PASSWORD_VERIFIED,
+    data: data
+  };
+}
+
+function errorPasswordVerified(data) {
+  return {
+    type: ERROR_PASSWORD_VERIFIED,
     data: data.errors
   };
 }
