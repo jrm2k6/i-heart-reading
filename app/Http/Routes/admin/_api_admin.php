b391e4a5397c3023563dd9536c8507e539b2d4d7
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-12-02 08:14:00 -0800

    fix route name

--- app/Http/Routes/admin/_api_admin.php
+++ app/Http/Routes/admin/_api_admin.php
@@ -3,5 +3,5 @@
 Route::group(['prefix' => 'api', 'middleware' => ['web', 'auth'], 'namespace' => 'Admin'], function() {
     Route::resource('teacher', 'AdminTeacherController');
     Route::resource('groups', 'AdminGroupController');
-    Route::resource('administator', 'AdminAdministratorController');
+    Route::resource('administrator', 'AdminAdministratorController');
 });
