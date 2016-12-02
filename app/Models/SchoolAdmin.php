2f141389e8b536f8e4f463d9be3da245a058752b
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-12-02 08:14:06 -0800

    import soft deletes

--- app/Models/SchoolAdmin.php
+++ app/Models/SchoolAdmin.php
@@ -1,8 +1,7 @@
-<?php
-
-namespace App\Models;
+<?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SchoolAdmin extends Model
 {
