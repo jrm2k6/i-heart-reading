d9f23a1c99d9cfa9a0fb1e2137c29cfa76495514
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-12-04 17:45:35 -0800

    remove unused factory

--- database/factories/AssignmentFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$factory->defineAs(App\Models\BookAssignment::class, 'default', function (Faker\Generator $faker) {
-    return [
-        'user_id' => 1,
-        'book_id' => 1
-    ];
-});
