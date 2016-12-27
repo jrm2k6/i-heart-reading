c1f0eff388a3f471f2cc87bd38d4959542a55a78
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-12-26 18:04:09 -0800

    show icons on hover event

--- resources/assets/js/components/admin/ListItemTeacher.js
+++ resources/assets/js/components/admin/ListItemTeacher.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 export default class ListItemTeacher extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovering: false
+    };
+  }
+
   render() {
+    const listItemOptions = (this.state.hovering) ? (
+      <div className='admin-list-item-option'>
+        <i className='material-icons admin-list-item-option-edit-icon'>
+          edit
+        </i>
+        <i className='material-icons admin-list-item-option-delete-icon'>
+          delete
+        </i>
+      </div>
+    ) : null;
+
     return (
-      <div className='admin-list-item'>
+      <div className='admin-list-item'
+        onMouseEnter={() => { this.setState({ hovering: true })}}
+        onMouseLeave={() => { this.setState({ hovering: false })}}
+      >
         <span>{this.props.teacher.user.name}</span>
         <span>{this.props.teacher.num_groups}</span>
+        {listItemOptions}
       </div>
     );
   }
