ad1943fe865100e9376348381e13d086a32a8d30
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-01-08 15:58:12 -0800

    add new routes for fetching students from group

--- app/Http/Routes/admin/_api_admin.php
+++ app/Http/Routes/admin/_api_admin.php
@@ -2,7 +2,8 @@
 
 Route::group(['prefix' => 'api', 'middleware' => ['web', 'auth'], 'namespace' => 'Admin'], function() {
     Route::resource('teacher', 'AdminTeacherController');
-    Route::get('group/{$id}/students', '\App\Http\Controllers\Signup\SchoolGroupController@getStudents');
+    Route::get('group/{id}/students', '\App\Http\Controllers\Signup\SchoolGroupController@getStudents');
+    Route::get('group/students/all', '\App\Http\Controllers\Signup\SchoolGroupController@getStudentsExcept');
     Route::resource('groups', 'AdminGroupController');
     Route::get('administrator/me', 'AdminAdministratorController@getAdminUser');
     Route::resource('administrator', 'AdminAdministratorController');
