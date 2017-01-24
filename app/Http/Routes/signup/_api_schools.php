0b9e08b2e205e810c48bac5151fd4892bd2763fc
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-01-24 09:13:57 -0800

    add middleware to signup routes

--- app/Http/Routes/signup/_api_schools.php
+++ app/Http/Routes/signup/_api_schools.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'api', 'middleware' => ['web'], 'namespace' => 'Signup'], function() {
+Route::group(['prefix' => 'api', 'middleware' => ['web', 'can_signup_as_organization'], 'namespace' => 'Signup'], function() {
     Route::resource('school/contact/verify', 'PrimaryContactController@verifyExists');
     Route::resource('school/contact', 'PrimaryContactController');
     Route::resource('school/group', 'SchoolGroupController');
