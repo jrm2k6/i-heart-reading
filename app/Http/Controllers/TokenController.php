a7e0aab7b1f7d0fbdeda202fe7932b6976fd22d2
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-01-24 09:13:12 -0800

    add token controller

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Models\SignupOrganizationToken;
+
+class TokenController extends Controller
+{
+    public function verifyOrganizationTokenAndRedirect(Request $request, $token)
+    {
+        $signupToken = SignupOrganizationToken::where('token', $token)->first();
+
+        if ($signupToken != null) {
+            $request->session()->put('organization_token', $signupToken->token);
+            return redirect()->action('SignupController@index');
+        }
+
+        return response()->view('errors.404', [], 404);
+    }
+}
