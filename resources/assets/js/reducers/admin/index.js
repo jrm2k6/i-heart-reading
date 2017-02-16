db13b0d0c1cc0815435e338623bec281a3b507d1
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-02-16 09:18:42 -0800

    use alert reducers as part of set of admin reducers

--- resources/assets/js/reducers/admin/index.js
+++ resources/assets/js/reducers/admin/index.js
@@ -1,5 +1,6 @@
 import adminReducer from './adminReducer';
 import userProfileReducer from '../userProfileReducer';
 import modalReducer from '../modals/modalReducer';
+import alertsReducer from '../alertsReducer';
 
-module.exports = { adminReducer, userProfileReducer, modalReducer };
+module.exports = { adminReducer, userProfileReducer, modalReducer, alertsReducer };
