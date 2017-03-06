c00848eea6176b3d07703915a8b50a8a57d7b192
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-03-05 18:17:54 -0800

    fix path when clicking on login button

--- resources/views/static/landing.blade.php
+++ resources/views/static/landing.blade.php
@@ -24,7 +24,7 @@
           <img class='landing-header-logo-img' src='/images/logos/i-heart-reading-logo.png' />
         </div>
         <div class='landing-header-menu'>
-          <div><a class='top-menu-item' href='/login'>Log in</a></div>
+          <div><a class='top-menu-item' href='/app'>Log in</a></div>
           <div><a class='top-menu-item' href='mailto:jeremy.dagorn@gmail.com?Subject=Hello!'>contact</a></div>
         </div>
       </div>
