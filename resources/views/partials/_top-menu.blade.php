b983baa7f205df561294b469c2e0a66dd8e9798f
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-03-19 15:39:29 -0700

    extract partial for top-menu

---
+++ resources/views/partials/_top-menu.blade.php
@@ -0,0 +1,9 @@
+<div class='landing-header'>
+    <div class='landing-header-logo'>
+        <img class='landing-header-logo-img' src='/images/logos/i-heart-reading-logo.png' />
+    </div>
+    <div class='landing-header-menu'>
+        <div><a class='top-menu-item' href='/app'>Log in</a></div>
+        <div><a class='top-menu-item' href='mailto:jeremy.dagorn@gmail.com?Subject=Hello!'>contact</a></div>
+    </div>
+</div>
