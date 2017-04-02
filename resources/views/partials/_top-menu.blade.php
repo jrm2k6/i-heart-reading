790d5e014661ca0b0f3745b31b67e55168f6c4ae
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2017-04-02 08:21:32 -0700

    update email address

--- resources/views/partials/_top-menu.blade.php
+++ resources/views/partials/_top-menu.blade.php
@@ -4,6 +4,6 @@
     </div>
     <div class='landing-header-menu'>
         <div><a class='top-menu-item' href='/app'>Log in</a></div>
-        <div><a class='top-menu-item' href='mailto:jeremy.dagorn@gmail.com?Subject=Hello!'>contact</a></div>
+        <div><a class='top-menu-item' href='mailto:support@iheartreading.co?Subject=Hello!'>contact</a></div>
     </div>
 </div>
--- resources/views/static/landing.blade.php
+++ resources/views/static/landing.blade.php
@@ -126,7 +126,7 @@
       </div>
       <div class='footer-content'>
         <div class='left-section-footer'>
-          <a class='contact' href="mailto:jeremy.dagorn@gmail.com?Subject=Hello!">Contact</a>
+          <a class='contact' href="mailto:support@iheartreading.co?Subject=Hello!">Contact</a>
         </div>
         <div class='right-section-footer'>
           Made with <span class='heart'>&hearts;</span> in San Diego.
